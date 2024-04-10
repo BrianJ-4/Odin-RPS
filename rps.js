dee148f8b8f03561d80050a92308f55533348235
BrianJ-4 <brian.joshua03@gmail.com>
2024-04-10 00:37:38 -0700

    Added function to get computers choice

---
+++ rps.js
@@ -0,0 +1,11 @@
+function getComputerChoice(){
+    choice = Math.floor(Math.random() * 3);
+    if(choice == 0){
+        return "rock";
+    }
+    else if(choice == 1){
+        return "paper";
+    }
+    return "scissors";
+}
+
