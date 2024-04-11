795aba40297518f80585a485ecf0cb5336a99f5d
BrianJ-4 <brian.joshua03@gmail.com>
2024-04-11 14:51:10 -0700

    Added playRound and playGame to make game playable in console

--- rps.js
+++ rps.js
@@ -9,3 +9,52 @@ function getComputerChoice(){
     return "scissors";
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection == computerSelection) {
+        return "Tie!";
+    }
+    
+    const outcomes = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper"
+    };
+
+    
+    if(computerSelection == outcomes[playerSelection]){
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+    else{
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
+
+function playGame(){
+    console.log("Game Starting");
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++){
+        let userChoice = prompt("Choose Rock, Paper, or Scissors");
+        let result = playRound(userChoice, getComputerChoice());
+        console.log(result);
+        if(result.includes("Win")){
+            playerScore++;
+        }
+        else if(result.includes("Lose")){
+            computerScore++;
+        }
+        console.log("Player: " + playerScore + "      Computer: " + computerScore);
+    }
+
+    if(playerScore > computerScore){
+        console.log("Player Wins!");
+    }
+    else{
+        console.log("Computer Wins!");
+    }
+}
+
+playGame()
